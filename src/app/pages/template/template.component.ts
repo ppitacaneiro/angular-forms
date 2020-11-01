ccc5297d015cb6c6e7127a131ab85d9c5e6f109d
pablo <ppitacaneiro@gmail.com>
2020-11-01 10:50:59 +0100

    Validaciones de formulario reactivo

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -9,7 +9,9 @@ import { NgForm } from '@angular/forms';
 export class TemplateComponent implements OnInit {
 
   usuario = {
-    nombre : 'Pablo'
+    nombre : '',
+    apellido : '',
+    correo : ''
   }
 
   constructor() { }
